8b80e44011f080f1c8402106673782013e959b01
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-03-29 10:26:42 -0500

    Created project

---
+++ Menu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DJP251
+{
+    internal class Menu
+    {
+        private Player player;
+        public void Execute()
+        {
+            player = GetPlayer();
+            ShowMenu();            
+        }
+
+        private Player GetPlayer()
+        {
+            string name;
+            int life;
+            int damage;
+
+            Console.WriteLine("Introduce tu nombre");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Introduce tu cantidad de vida");
+            life = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce tu cantidad de daño");
+            damage = int.Parse(Console.ReadLine());
+
+            return new Player(name, life, damage);
+        }
+
+        private void ShowMenu()
+        {
+            bool continueFlag = true;
+
+            while (continueFlag)
+            {
+                Console.WriteLine(player.GetData());
+                Console.WriteLine("1. Tomar poción (+10 vida)");
+                Console.WriteLine("2. Agarrar el cactus con la mano (-5 vida)");
+                Console.WriteLine("0. Salir");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        player.TakePotion(10);
+                        ShowStatus();
+                        break;
+                    case "2":
+                        player.TakeDamage(5);
+                        ShowStatus();
+                        break;
+                    case "0":
+                        continueFlag = false;
+                        break;
+                    default:
+                        Console.WriteLine("La opción no es válida");
+                        break;
+                }
+            }
+        }
+
+        private void ShowStatus()
+        {
+            if (player.Life > 0)
+            {
+                Console.WriteLine("Aún estás vivo");
+            }
+            else
+            {
+                Console.WriteLine("Te moriste");
+            }
+        }
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DJP251
+{
+    internal class Player
+    {
+        private string name;
+        private int life;
+        private int damage;
+
+        public string Name { get { return name; } }
+        public int Life { get { return life; } }
+        public int Damage { get { return damage; } }
+
+        public Player(string name, int life, int damage)
+        {
+            this.name = name;
+            this.life = life;
+            this.damage = damage;
+        }
+
+        public string GetData()
+        {
+            return $"Nombre: {name} - Vida: {life} - Daño: {damage}";
+        }
+
+        private void ChangeLife(int value)
+        {
+            life += value;
+            if (life <= 0)
+            {
+                life = 0;
+            }
+        }
+
+        public void TakePotion(int value)
+        {
+            ChangeLife(value);
+        }
+
+        public void TakeDamage(int value)
+        {
+            ChangeLife(-value);
+        }
+    }
+}
