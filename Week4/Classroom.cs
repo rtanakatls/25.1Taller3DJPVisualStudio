452a717e8a338482ff073ac17152651b9c34f98f
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-19 09:51:02 -0500

    Added classroom

---
+++ Week4/Classroom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DJP251.Week4
+{
+    internal class Classroom
+    {
+        protected List<Student> students;
+        protected List<Teacher> teacher;
+    }
+}
